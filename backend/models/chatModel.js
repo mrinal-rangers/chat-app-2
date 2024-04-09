217cb361e8b79e97705a58263814cd35da7dff00
Mrinal Deb <mrinal437a@gmail.com>
2024-04-09 23:50:58 +0530

    sixth

---
+++ backend/models/chatModel.js
@@ -0,0 +1,36 @@
+//chatName 
+// isGroupChat
+// users
+// latestMessaage
+// Admin
+
+const mongoose = require('mongoose');
+
+const chatModel = mongoose.Schema({
+    chatName:{
+        type:String,
+        trim:true
+    },
+    isGroupChat:{
+        type:Boolean,
+        default:false,
+    },
+    users :[{
+        type: mongoose.Schema.Types.ObjectId ,
+        ref:"User",
+    }],
+    lastestMessage:{
+        type: mongoose.Schema.Types.ObjectId ,
+        ref:"Message",
+    },
+    groupAdmin:{
+        type: mongoose.Schema.Types.ObjectId ,
+        ref:"User",
+    }
+},{
+    timestamps:true,
+})
+
+const Chat = mongoose.model("chatModel",chatModel);
+
+module.exports = Chat;
