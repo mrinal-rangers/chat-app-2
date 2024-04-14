5356df09f99b5687addf97bc004cb50a30e8949d
Mrinal Deb <mrinal437a@gmail.com>
2024-04-15 04:36:07 +0530

    thoda bahut fix hogaya messagecontrollers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 const userRoutes = require('./routes/userRoutes')
 const {notFound,errorHandler} = require('./middlewares/errorMiddleware')
 const chatRoutes = require('./routes/chatRoutes')
+const messageRoutes = require('./routes/messageRoutes')
 
 const app = express();
 dotenv.config();
@@ -19,6 +20,7 @@ app.get('/',(req,res)=>{
 })
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
+app.use("/api/message", messageRoutes);
 
 
 
